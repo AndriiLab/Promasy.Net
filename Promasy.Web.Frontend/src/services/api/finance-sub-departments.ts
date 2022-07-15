46425b95d2a11536247805ee1ecce44c2f4d34b6
Andrii Labyntsev <lab.andrey@gmail.com>
2022-07-15 22:15:20 +0200

    Add Finances API to frontend

---
+++ Promasy.Web.Frontend/src/services/api/finance-sub-departments.ts
@@ -0,0 +1,73 @@
+﻿import { ErrorApiResponse, Fetch, PagedResponse, Response, SelectItem } from "@/utils/fetch-utils";
+import { buildQueryParameters } from "@/utils/url-params-utils";
+
+export default {
+  getFundTypes(): Promise<Response<SelectItem<number>[], ErrorApiResponse>> {
+    return Fetch.Get<SelectItem<number>[], ErrorApiResponse>("/api/finances/all-fund-types");
+  },
+  getList(
+    financeSourceId: number,
+    page: number,
+    offset: number,
+    search?: string,
+    order?: string,
+    descending?: boolean
+  ): Promise<Response<PagedResponse<FinanceSubDepartment>, ErrorApiResponse>> {
+    return Fetch.Get<PagedResponse<FinanceSubDepartment>, ErrorApiResponse>(
+      `/api/finances/${financeSourceId}/sub-departments${buildQueryParameters([
+        ["offset", offset.toString()],
+        ["page", page.toString()],
+        ["search", search],
+        ["order", order],
+        ["desc", descending ? "true" : undefined],
+      ])}`
+    );
+  },
+  getByFinanceAndSubDepartmentId(subDepartmentId: number, financeSourceId: number) : Promise<Response<FinanceSubDepartment, ErrorApiResponse>> {
+    return Fetch.Get<FinanceSubDepartment, ErrorApiResponse>(`/api/finances/${ financeSourceId }/sub-departments/${subDepartmentId}`);
+  },
+  create(request: CreateFinanceSubDepartmentRequest) : Promise<Response<FinanceSubDepartment, ErrorApiResponse>> {
+    return Fetch.Post<FinanceSubDepartment, ErrorApiResponse>(`/api/finances/${ request.financeSourceId }/sub-departments/`, { body: JSON.stringify(request) });
+  },
+  update(request: UpdateFinanceSubDepartmentRequest) : Promise<Response<string, ErrorApiResponse>> {
+    return Fetch.Put<string, ErrorApiResponse>(`/api/finances/${ request.financeSourceId }/sub-departments/${request.subDepartmentId}`, { body: JSON.stringify(request) });
+  },
+  delete(subDepartmentId: number, financeSourceId: number) : Promise<Response<string, ErrorApiResponse>> {
+    return Fetch.Delete<string, ErrorApiResponse>(`/api/finances/${ financeSourceId }/sub-departments/${subDepartmentId}`);
+  },
+};
+
+export interface FinanceSubDepartment {
+  id: number;
+  financeSourceId: number;
+  subDepartmentId: number;
+  subDepartment: string;
+  departmentId: number;
+  department: string;
+  totalEquipment: string;
+  spentEquipment: string;
+  totalMaterials: string;
+  spentMaterials: string;
+  totalServices: string;
+  spentServices: string;
+  editorId: number;
+  editor: string;
+  editedDate: Date;
+}
+
+export interface CreateFinanceSubDepartmentRequest {
+  financeSourceId: number;
+  subDepartmentId: number;
+  totalEquipment: string;
+  totalMaterials: string;
+  totalServices: string;
+}
+
+export interface UpdateFinanceSubDepartmentRequest {
+  id: number;
+  financeSourceId: number;
+  subDepartmentId: number;
+  totalEquipment: string;
+  totalMaterials: string;
+  totalServices: string;
+}
---
+++ Promasy.Web.Frontend/src/services/api/finances.ts
@@ -0,0 +1,83 @@
+﻿import { ErrorApiResponse, Fetch, PagedResponse, Response, SelectItem } from "@/utils/fetch-utils";
+import { buildQueryParameters } from "@/utils/url-params-utils";
+
+export default {
+  getFundTypes(): Promise<Response<SelectItem<number>[], ErrorApiResponse>> {
+    return Fetch.Get<SelectItem<number>[], ErrorApiResponse>("/api/finances/all-fund-types");
+  },
+  getList(
+    year: number,
+    page: number,
+    offset: number,
+    search?: string,
+    order?: string,
+    descending?: boolean
+  ): Promise<Response<PagedResponse<FinanceSource>, ErrorApiResponse>> {
+    return Fetch.Get<PagedResponse<FinanceSource>, ErrorApiResponse>(
+      `/api/finances${buildQueryParameters([
+        ["year", year.toString()],
+        ["offset", offset.toString()],
+        ["page", page.toString()],
+        ["search", search],
+        ["order", order],
+        ["desc", descending ? "true" : undefined],
+      ])}`
+    );
+  },
+  getById(id: number) : Promise<Response<FinanceSource, ErrorApiResponse>> {
+    return Fetch.Get<FinanceSource, ErrorApiResponse>(`/api/finances/${id}`);
+  },
+  create(request: CreateFinanceSourceRequest) : Promise<Response<FinanceSource, ErrorApiResponse>> {
+    return Fetch.Post<FinanceSource, ErrorApiResponse>("/api/finances", { body: JSON.stringify(request) });
+  },
+  update(request: UpdateFinanceSourceRequest) : Promise<Response<string, ErrorApiResponse>> {
+    return Fetch.Put<string, ErrorApiResponse>(`/api/finances/${request.id}`, { body: JSON.stringify(request) });
+  },
+  delete(id: number) : Promise<Response<string, ErrorApiResponse>> {
+    return Fetch.Delete<string, ErrorApiResponse>(`/api/finances/${id}`);
+  },
+};
+
+export interface FinanceSource {
+  id: number;
+  number: string;
+  name: string;
+  fundType: number;
+  start: Date;
+  end: Date;
+  kpkvk: string;
+  totalEquipment: string;
+  spentEquipment: string;
+  totalMaterials: string;
+  spentMaterials: string;
+  totalServices: string;
+  spentServices: string;
+  editorId: number;
+  editor: string;
+  editedDate: Date;
+}
+
+export interface CreateFinanceSourceRequest {
+  number: string;
+  name: string;
+  fundType: number;
+  start: Date;
+  end: Date;
+  kpkvk: string;
+  totalEquipment: string;
+  totalMaterials: string;
+  totalServices: string;
+}
+
+export interface UpdateFinanceSourceRequest {
+  id: number;
+  number: string;
+  name: string;
+  fundType: number;
+  start: Date;
+  end: Date;
+  kpkvk: string;
+  totalEquipment: string;
+  totalMaterials: string;
+  totalServices: string;
+}
